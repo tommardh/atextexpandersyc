0834ee6161ec1ac70dd0672eb46b6bacef5237db
Tom Mårdh <tom.mardh@softhouse.se>
2016-01-08 13:02:55 +0100

    Added possibility to add expandergroup

--- atextexpandersync.js
+++ atextexpandersync.js
@@ -2,14 +2,33 @@ Expandergroups = new Mongo.Collection("expandergroups")
 
 if (Meteor.isClient) {
   
-  Session.setDefault('counter', 0);
-
   Template.body.helpers({
     expandergroups: function () {
-      return Expandergroups.find ({});
+      return Expandergroups.find ({}, {sort: {text: 1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-expandergroup": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Expandergroups.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
     }
   });
 }
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
