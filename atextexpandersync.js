7038fd293a7c5d429d23b2a818542121880358fb
Tom Mårdh <tom.mardh@softhouse.se>
2016-01-08 15:01:41 +0100

    Added inactive filter

--- atextexpandersync.js
+++ atextexpandersync.js
@@ -3,7 +3,19 @@ Expandergroups = new Mongo.Collection("expandergroups")
 if (Meteor.isClient) {
   Template.body.helpers({
     expandergroups: function () {
-      return Expandergroups.find ({}, {sort: {text: 1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Expandergroups.find({checked: {$ne: true}}, {sort: {text: 1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Expandergroups.find({}, {sort: {text: 1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+     },
+    incompleteCount: function () {
+      return Expandergroups.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -23,6 +35,9 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
